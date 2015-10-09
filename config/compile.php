a059aa2f7ad54e42c92740c6bcc23410b743bd8e
freek <freek@spatie.be>
2015-10-09 22:18:03 +0200

    fix insights issue

--- config/compile.php
+++ config/compile.php
@@ -14,7 +14,7 @@ return [
 	*/
 
 	'files' => [
-		
+
 		realpath(__DIR__.'/../app/Providers/BusServiceProvider.php'),
 		realpath(__DIR__.'/../app/Providers/ConfigServiceProvider.php'),
 		realpath(__DIR__.'/../app/Providers/EventServiceProvider.php'),
