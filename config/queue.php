70687c4a34375840a4b25da05d4771a5842d5830
freek <freek@spatie.be>
2015-10-09 22:17:00 +0200

    fix composer update

--- config/queue.php
+++ config/queue.php
@@ -16,7 +16,7 @@ return [
 	|
 	*/
 
-	'default' => env('QUEUE_DRIVER'),
+	'default' => env('QUEUE_DRIVER', 'sync'),
 
 	/*
 	|--------------------------------------------------------------------------
