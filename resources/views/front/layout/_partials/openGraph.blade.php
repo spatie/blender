cc5a9dd1b9b721494ae574ff9758dee35eb3ad92
Willem Van Bockstal <willem@spatie.be>
2015-10-29 11:46:49 +0100

    og-image

--- resources/views/front/layout/_partials/openGraph.blade.php
+++ resources/views/front/layout/_partials/openGraph.blade.php
@@ -4,3 +4,5 @@
 <meta property="og:title" content="@yield('pageTitle')" />
 <meta property="og:description" content="@yield('pageDescription')" />
 <meta property="og:image" content="{{ Request::root() }}/images/og-image.png" />
+<meta property="og:image:width" content="1600">
+<meta property="og:image:height" content="800">
