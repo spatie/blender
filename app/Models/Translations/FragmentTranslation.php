baf9be38fd31a1241213d2a4786dbfe8e04d6276
Freek Van der Herten <freek@spatie.be>
2015-12-07 10:25:29 +0100

    Update FragmentTranslation.php

--- app/Models/Translations/FragmentTranslation.php
+++ app/Models/Translations/FragmentTranslation.php
@@ -8,13 +8,5 @@ use Cache;
 
 class FragmentTranslation extends Translation
 {
-    public static function boot()
-    {
-        parent::boot();
 
-        static::updating(function ($stringTranslation) {
-            $string = Fragment::findOrFail($stringTranslation->fragment_id);
-            Cache::forget(Fragment::getCacheKeyName($string->name, $stringTranslation->locale));
-        });
-    }
 }
