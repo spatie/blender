66773dfd6eb9bedffb3a5056e237052a588d8cd5
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2015-12-16 11:23:58 +0100

    Bugsnag

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,9 +3,8 @@
 namespace App\Exceptions;
 
 use Exception;
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-
-// use Bugsnag\BugsnagLaravel\BugsnagExceptionHandler as ExceptionHandler;
+//use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Bugsnag\BugsnagLaravel\BugsnagExceptionHandler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
