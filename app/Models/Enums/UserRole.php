8320f89b49a4e1f6f8630e12ad2b97591e7b0d0f
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2015-12-28 10:58:54 +0100

    Enums

--- app/Models/Enums/UserRole.php
+++ app/Models/Enums/UserRole.php
@@ -9,7 +9,7 @@ class UserRole extends Enum
     const ADMIN = 'admin';
     const MEMBER = 'member';
 
-    public static function allAsRegex()
+    public static function allAsRegex() : string
     {
         return collect(self::values())->map(function ($role) {
             return "({$role})";
