7da9c959abab7f667422bfbd1840003d79265d26
Jolita Grazyte <jolita.grazyte@student.kdg.be>
2016-03-18 11:16:58 +0100

    model cleanup

--- app/Foundation/Models/Base/ModuleModel.php
+++ app/Foundation/Models/Base/ModuleModel.php
@@ -6,8 +6,11 @@ use App\Foundation\Models\Traits\Draftable;
 use App\Foundation\Models\Traits\Presentable;
 use App\Foundation\Models\Traits\HasMedia as HasMediaTrait;
 use Spatie\MediaLibrary\HasMedia\Interfaces\HasMediaConversions;
+use Spatie\ModelCleanup\GetsCleanedUp;
+use Illuminate\Database\Eloquent\Builder;
+use Carbon\Carbon;
 
-abstract class ModuleModel extends TranslatableEloquent implements HasMediaConversions
+abstract class ModuleModel extends TranslatableEloquent implements HasMediaConversions, GetsCleanedUp
 {
     use Draftable, Presentable, HasMediaTrait;
 
@@ -24,5 +27,12 @@ abstract class ModuleModel extends TranslatableEloquent implements HasMediaConve
             ->setWidth(368)
             ->setHeight(232)
             ->nonQueued();
+    }
+
+    public static function cleanUp(Builder $query) : Builder
+    {
+        return $query
+            ->draft()
+            ->where('created_at', '<', Carbon::now()->subWeek());
     }
 }
