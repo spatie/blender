d8be5dc746bce8d824465eb07314bd513ceb447c
Willem Van Bockstal <willem@spatie.be>
2016-03-18 09:44:58 +0100

    back menu

--- resources/views/back/layout/_partials/menu.blade.php
+++ resources/views/back/layout/_partials/menu.blade.php
@@ -1,20 +1,20 @@
 <div class="menu">
     <div class="grid">
         <div class="clearfix">
-            <span class="menu_front">
+            <nav class="menu_front">
                 <a class="menu_front_link" href="{{ URL::to('/') }}" target="blender">
                     <span class="menu_circle -front"></span>
                     <span class="menu_front_link_host">{{ Request::getHost() }}</span>
                     <span class="menu_front_link_protocol"><span class="fa {{ Request::isSecure() ? 'fa-lock': 'fa-unlock' }}"></span></span>
                 </a>
-            </span>
+            </nav>
             <nav class="menu_user">
                 {!! Navigation::getBackUserMenu() !!}
             </nav>
         </div>
         <nav class="menu_main" data-menu-main>
-            <ul class="menu_groups" data-menu-groups="single">
-                <li class="menu_group">
+            <ul class="menu_groups" data-menu-groups>
+                <li class="menu_group -single">
                     {!! Navigation::getBackDashboardMenu() !!}
                 </li>
                 <li class="menu_group" data-menu-group="inhoud">
