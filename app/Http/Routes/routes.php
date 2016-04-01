c55f023fff86b3cf1670b90cbb2327d73e0bc68c
freek <freek@spatie.be>
2016-04-01 22:33:35 +0200

    fix issue reported by our dutch human scrutinizer

--- app/Http/Routes/routes.php
+++ app/Http/Routes/routes.php
@@ -1,33 +1,32 @@
 <?php
 
+Route::demoAccess('/demo');
+
 Route::group(['namespace' => 'Back', 'prefix' => 'blender'], function () {
 
-    require __DIR__.'/back_auth.php';
+    require __DIR__ . '/back_auth.php';
 
     Route::group(['middleware' => 'auth'], function () {
-        require __DIR__.'/back.php';
+        require __DIR__ . '/back.php';
     });
 });
 
 Route::group(['namespace' => 'Front', 'middleware' => 'demoMode'], function () {
 
-        $multiLingual = count(config('app.locales')) > 1;
+    $multiLingual = count(config('app.locales')) > 1;
 
-        Route::demoAccess('/demo');
-
-        Route::group($multiLingual ? ['prefix' => locale()] : [], function () {
-            try {
-                require __DIR__.'/front_auth.php';
-                require __DIR__.'/front.php';
-            } catch (Exception $exception) {
-                logger()->warning('Front routes weren\'t included.');
-            }
-        });
-
-        if ($multiLingual) {
-            Route::get('/', function () {
-                return redirect(locale());
-            });
+    Route::group($multiLingual ? ['prefix' => locale()] : [], function () {
+        try {
+            require __DIR__ . '/front_auth.php';
+            require __DIR__ . '/front.php';
+        } catch (Exception $exception) {
+            logger()->warning('Front routes weren\'t included.');
         }
+    });
+
+    if ($multiLingual) {
+        Route::get('/', function () {
+            return redirect(locale());
+        });
     }
-);
+});
