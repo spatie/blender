55a4b4ae3c9575881ba63c1421370b4fd4b8d266
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-05-04 09:46:06 +0200

    LoginAs

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \App\Http\Middleware\LoginAs::class,
         \App\Http\Middleware\VerifyCsrfToken::class,
         \App\Http\Middleware\RobotsMiddleware::class,
         \App\Http\Middleware\SanitizeInput::class,
