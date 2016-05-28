d3b3e4790aeea473806f4306ac86fff92b3c6c99
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-05-28 18:04:06 +0200

    Detect dominant color

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Services\Mailers\AdminMailerEventHandler;
 use App\Services\Mailers\MemberMailerEventHandler;
+use App\Services\Medialibrary\MediaLibraryEventHandler;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -11,5 +12,6 @@ class EventServiceProvider extends ServiceProvider
     protected $subscribe = [
         AdminMailerEventHandler::class,
         MemberMailerEventHandler::class,
+        MediaLibraryEventHandler::class,
     ];
 }
