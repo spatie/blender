b10db59b2e7fd5a482fda50e32418445e7118a63
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-06-07 10:58:42 +0200

    Bitbucket => GitHub

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -68,7 +68,7 @@ cd {{ $releasesDir }};
 mkdir {{ $newReleaseDir }};
 
 # Clone the repo
-git clone --depth 1 git@bitbucket.org:{{ $repository }} {{ $newReleaseName }}
+git clone --depth 1 git@github.com:{{ $repository }} {{ $newReleaseName }}
 
 # Configure sparse checkout
 cd {{ $newReleaseDir }}
