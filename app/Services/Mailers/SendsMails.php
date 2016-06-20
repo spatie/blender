0925b891f44b563a4926ee42298a810d9dc14cfc
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-06-20 14:23:59 +0200

    Allow users to be passed to sendMail

--- app/Services/Mailers/SendsMails.php
+++ app/Services/Mailers/SendsMails.php
@@ -8,13 +8,17 @@ use Illuminate\Mail\Message;
 trait SendsMails
 {
     /**
-     * @param mixed $addresses
+     * @param string|array|\App\Services\Auth\User $addresses
      * @param string $subject
      * @param string $view
      * @param mixed $data
      */
     public function sendMail($addresses, string $subject, string $view, $data = [])
     {
+        if ($addresses instanceof User) {
+            $addresses = $addresses->email;
+        }
+        
         collect($addresses)->each(function (string $email) use ($subject, $view, $data) {
             app(Mailer::class)->queue($view, $data, function (Message $message) use ($email, $subject) {
                 $message->to($email)->subject($subject);
