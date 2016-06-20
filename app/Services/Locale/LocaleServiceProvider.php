e6a22b81dced9e8e35b1ac45f703cda95ae8aed9
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-06-20 13:27:35 +0200

    Simplified LocaleServiceProvider

--- app/Services/Locale/LocaleServiceProvider.php
+++ app/Services/Locale/LocaleServiceProvider.php
@@ -2,29 +2,16 @@
 
 namespace App\Services\Locale;
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Translation\Translator;
+use Illuminate\Translation\TranslationServiceProvider;
 
-class LocaleServiceProvider extends ServiceProvider
+class LocaleServiceProvider extends TranslationServiceProvider
 {
     public function register()
     {
         $this->app->singleton(CurrentLocale::class);
         $this->app->alias(CurrentLocale::class, 'currentLocale');
 
-        $this->registerLoader();
-
-        $this->app->singleton('translator', function ($app) {
-            $loader = $app['translation.loader'];
-
-            $locale = $app['config']['app.locale'];
-
-            $trans = new Translator($loader, $locale);
-
-            $trans->setFallback($app['config']['app.fallback_locale']);
-
-            return $trans;
-        });
+        parent::register();
     }
 
     protected function registerLoader()
@@ -32,10 +19,5 @@ class LocaleServiceProvider extends ServiceProvider
         $this->app->singleton('translation.loader', function ($app) {
             return new TranslationLoader($app['files'], $app['path.lang']);
         });
-    }
-
-    public function provides(): array
-    {
-        return ['translator', 'translation.loader'];
     }
 }
