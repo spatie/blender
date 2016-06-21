45300741a84cd980d3fe2de792d7ac6e4661928a
freek <freek@spatie.be>
2016-06-21 15:35:37 +0200

    update

--- config/laravel-analytics.php
+++ config/laravel-analytics.php
@@ -17,6 +17,6 @@ return [
      * The amount of minutes the Google API responses will be cached.
      * If you set this to zero, the responses won't be cached at all.
      */
-    'cache_lifetime_in_minutes' => 60 * 24,
+    'cache_lifetime_in_minutes' => (60 * 24) - 5,
 
 ];
