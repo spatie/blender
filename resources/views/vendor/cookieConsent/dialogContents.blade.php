ba6abec051c0c3bcf5354c834e1c01ce3a16b628
freek <freek@spatie.be>
2016-07-04 15:19:10 +0200

    use fragments for cookieconsent

---
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -0,0 +1,10 @@
+<div class="js-cookie-consent cookie-consent">
+
+    <span class="cookie-consent__message">
+        {!! fragment('cookieConsent.message') !!}
+    </span>
+
+    <button class="js-cookie-consent-agree cookie-consent__agree">
+        {{ fragment('cookieConsent.agree') }}
+    </button>
+</div>
