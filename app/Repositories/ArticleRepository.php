75d6d29c5eda0f48285e3f70c7b27fc7ced5cfea
Freek Van der Herten <freek@spatie.be>
2016-11-07 12:21:02 +0100

    Applied fixes from StyleCI (#106)

--- app/Repositories/ArticleRepository.php
+++ app/Repositories/ArticleRepository.php
@@ -18,7 +18,7 @@ class ArticleRepository
 
     public static function findBySlug(string $url): Article
     {
-        $article =  Article::online()
+        $article = Article::online()
             ->where('url->'.content_locale(), $url)
             ->first();
 
