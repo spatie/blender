10291c8670459f0c4c4b327da7a5c4b1c695c944
Willem Van Bockstal <willem@spatie.be>
2016-11-21 09:23:14 +0100

    Update vendor Sass

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -1,9 +1,7 @@
 <div class="js-cookie-consent cookie-consent">
-
     <span class="cookie-consent__message">
         {!! fragment('cookieConsent.message') !!}
     </span>
-
     <button class="js-cookie-consent-agree cookie-consent__agree">
         {{ fragment('cookieConsent.agree') }}
     </button>
