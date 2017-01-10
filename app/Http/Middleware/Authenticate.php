807619af2335af755c605a9610e060982b3cc8ff
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-01-10 17:47:44 +0100

    Authenticate

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,17 +3,11 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use App\Services\Auth\Back\User;
 
 class Authenticate
 {
     public function handle($request, Closure $next)
     {
-        if (app()->environment() === 'production') {
-            die('Remove Auth::login(User::first() from Authenticate middleware');
-        }
-        \Auth::login(User::first());
-
         if (! current_user()) {
             return $this->handleUnauthorizedRequest($request);
         }
