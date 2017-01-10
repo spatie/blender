1f1902d6b4c181cb327d5272bc159e30716bda47
freek <freek@spatie.be>
2017-01-10 16:59:20 +0100

    man, this has been bothering me for several year. Glad this is finally fixed

--- resources/views/front/_layouts/_partials/footer.blade.php
+++ resources/views/front/_layouts/_partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer h-padding h-align-center">
     <small>
-        © {{ Date('Y') }} <a href="https://spatie.be">spatie.be webdesign, Antwerpen</a>
+        © {{ date('Y') }} <a href="https://spatie.be">spatie.be webdesign, Antwerpen</a>
     </small>
 </footer>
