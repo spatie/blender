1fc830c9c390471687b450f1ffc4bbabde0cc5ae
freek <freek@spatie.be>
2017-01-24 16:20:55 +0100

    refactor route service provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,9 +22,9 @@ class RouteServiceProvider extends ServiceProvider
     protected function registerMacros(Router $router)
     {
         $router->macro('module', function ($module, $sortable = false) use ($router) {
-            $controller = ucfirst($module).'Controller';
+            $controller = ucfirst($module) . 'Controller';
             if ($sortable) {
-                $router->patch("{$module}/changeOrder", $controller.'@changeOrder');
+                $router->patch("{$module}/changeOrder", $controller . '@changeOrder');
             }
 
             $router->resource($module, $controller);
@@ -51,42 +51,50 @@ class RouteServiceProvider extends ServiceProvider
             /*
              * Back site
              */
-            Route::group(['namespace' => 'Back', 'middleware' => 'web', 'prefix' => 'blender'], function () {
-                Auth::routes();
+            Route::prefix('blender')
+                ->middleware('web')
+                ->namespace('Back')
+                ->group(function () {
+                    Auth::routes();
 
-                Route::group(['middleware' => 'auth'], function () {
-                    require base_path('routes/back.php');
+                    Route::middleware('auth')->group(function () {
+                        require base_path('routes/back.php');
+                    });
                 });
-            });
 
             /*
              * Frontsite
              */
 
-            Route::group(['namespace' => 'Front'], function () {
-                Route::group(['namespace' => 'Api', 'middleware' => 'api', 'prefix' => 'api'], function () {
-                    require base_path('routes/frontApi.php');
-                });
+            Route::namespace('Front')
+                ->group(function () {
 
-                Route::group(['middleware' => ['web', 'demoMode', 'rememberLocale']], function () {
-                    $multiLingual = count(config('app.locales')) > 1;
+                    Route::prefix('api')
+                        ->middleware('api')
+                        ->namespace('Api')
+                        ->group(function () {
+                            require base_path('routes/frontApi.php');
+                        });
 
-                    Route::group($multiLingual ? ['prefix' => locale()] : [], function () {
-                        try {
-                            Auth::routes();
-                            require base_path('routes/front.php');
-                        } catch (Exception $exception) {
-                            logger()->warning("Front routes weren't included because {$exception->getMessage()}.");
+                    Route::middleware(['web', 'demoMode', 'rememberLocale'])->group(function () {
+                        $multiLingual = count(config('app.locales')) > 1;
+
+                        Route::group($multiLingual ? ['prefix' => locale()] : [], function () {
+                            try {
+                                Auth::routes();
+                                require base_path('routes/front.php');
+                            } catch (Exception $exception) {
+                                logger()->warning("Front routes weren't included because {$exception->getMessage()}.");
+                            }
+                        });
+
+                        if ($multiLingual) {
+                            Route::get('/', function () {
+                                return redirect(locale());
+                            });
                         }
                     });
-
-                    if ($multiLingual) {
-                        Route::get('/', function () {
-                            return redirect(locale());
-                        });
-                    }
                 });
-            });
         });
     }
 }
