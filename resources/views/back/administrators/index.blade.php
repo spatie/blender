44750e39226ef110522c342ac405b9f35aee4ece
freek <freek@spatie.be>
2017-01-25 13:20:05 +0100

    wip

--- resources/views/back/administrators/index.blade.php
+++ resources/views/back/administrators/index.blade.php
@@ -2,43 +2,44 @@
     'pageTitle' => fragment('back.administrators.title')
 ])
 
-    <section>
-        <div class="grid">
-            <h1>{{ fragment('back.administrators.title') }}</h1>
-            <a href="{{ action('Back\AdministratorsController@create') }}" class="button">
-                {{ fragment('back.administrators.new') }}
-            </a>
-            <table data-datatable data-order='[[ 0, "asc" ]]'>
-                <thead>
+<section>
+    <div class="grid">
+        <h1>{{ fragment('back.administrators.title') }}</h1>
+        <a href="{{ action('Back\AdministratorsController@create') }}" class="button">
+            {{ fragment('back.administrators.new') }}
+        </a>
+        <table data-datatable data-order='[[ 0, "asc" ]]'>
+            <thead>
+            <tr>
+                <th>E-mail</th>
+                <th>{{ __('Naam') }}</th>
+                <th>{{ __('Laatste activiteit') }}</th>
+                <th data-orderable="false"></th>
+            </tr>
+            </thead>
+            <tbody>
+
+            @foreach($users as $user)
                 <tr>
-                    <th>E-mail</th>
-                    <th>{{ __('Naam') }}</th>
-                    <th>{{ __('Laatste activiteit') }}</th>
-                    <th data-orderable="false"></th>
+                    <td>
+                        <a href="{{ action('Back\AdministratorsController@edit', [$user->id]) }}">{{ $user->email }}</a>
+                    </td>
+                    <td>
+                        {!! Html::avatar($user, '-small') !!} <span>{{ $user->name }}</span>
+                    </td>
+                    <td>
+                        {{ $user->lastActivityDate }}
+                    </td>
+                    <td class="-right">
+                        @unless ($user->isCurrentUser())
+                            {!! Html::deleteButton(action('Back\AdministratorsController@destroy', $user->id)) !!}
+                        @endunless
+                    </td>
                 </tr>
-                </thead>
-                <tbody>
-                @foreach($users as $user)
-                    <tr>
-                        <td>
-                            <a href="{{ action('Back\AdministratorsController@edit', [$user->id]) }}">{{ $user->email }}</a>
-                        </td>
-                        <td>
-                            {!! Html::avatar($user, '-small') !!} <span>{{ $user->name }}</span>
-                        </td>
-                        <td>
-                            {{ $user->lastActivityDate }}
-                        </td>
-                        <td class="-right">
-                            @unless ($user->isCurrentUser())
-                                {!! Html::deleteButton(action('Back\AdministratorsController@destroy', $user->id)) !!}
-                            @endunless
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
-    </section>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</section>
 
 @endcomponent
