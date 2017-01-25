6ec18c776ef035f41c3ab0a88fb6a2fc144b2513
Freek Van der Herten <freek@spatie.be>
2017-01-25 09:37:15 +0100

    Apply fixes from StyleCI (#194)

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
+use Spatie\Tags\Tag as SpatieTag;
 use Illuminate\Support\Facades\DB;
 use Spatie\EloquentSortable\Sortable;
-use Spatie\EloquentSortable\SortableTrait;
-use Spatie\Tags\Tag as SpatieTag;
 use Spatie\Blender\Model\Traits\Draftable;
+use Spatie\EloquentSortable\SortableTrait;
 
 class Tag extends SpatieTag implements Sortable
 {
