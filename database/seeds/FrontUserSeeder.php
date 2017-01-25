796e2d164a39fc3018c3969845a8baf52f99965f
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-01-25 09:39:20 +0100

    Apply factories PR

--- database/seeds/FrontUserSeeder.php
+++ database/seeds/FrontUserSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Services\Auth\Front\User;
+use App\Services\Auth\Front\Enums\UserRole;
 use App\Services\Auth\Front\Enums\UserStatus;
 
 class FrontUserSeeder extends DatabaseSeeder
@@ -9,34 +10,49 @@ class FrontUserSeeder extends DatabaseSeeder
     {
         $this->truncate((new User())->getTable());
 
-        $this->seedSpatieUsers();
-        $this->seedRandomFrontUsers();
-    }
+        collect([
+            ['Freek', 'Van der Herten'],
+            ['Jef', 'Van der Voort'],
+            ['Rogier', 'De Boevé'],
+            ['Sebastian', 'De Deyne'],
+            ['Willem', 'Van Bockstal'],
+        ])->each(function ($name) {
+            [$firstName, $lastName] = $name;
 
-    public function seedRandomFrontUsers($amount = 10)
-    {
-        return factory(User::class, $amount)->create([]);
-    }
-
-    public function seedSpatieUsers()
-    {
-        $users = [
-            'Willem' => 'Van Bockstal',
-            'Freek' => 'Van der Herten',
-            'Rogier' => 'De Boevé',
-            'Sebastian' => 'De Deyne',
-        ];
-
-        collect($users)->each(function ($lastName, $firstName) {
-            $password = app()->environment('local') ? strtolower($firstName) : string()->random();
-
-            factory(User::class)->create([
-                'email' => strtolower($firstName).'@spatie.be',
-                'password' => bcrypt($password),
-                'first_name' => "{$firstName}",
+            $this->createFrontUser([
+                'first_name' => $firstName,
                 'last_name' => $lastName,
+                'email' => strtolower($firstName).'@spatie.be',
+                'password' => bcrypt(strtolower($firstName)),
                 'status' => UserStatus::ACTIVE,
             ]);
         });
+
+        collect()->range(0, 10)->each(function () {
+            $this->createFrontUser();
+        });
+    }
+
+    public function createFrontUser(array $attributes = []): User
+    {
+        $person = faker()->person();
+
+        return User::create($attributes + [
+            'first_name' => $person['firstName'],
+            'last_name' => $person['lastName'],
+            'email' => $person['email'],
+            'password' => bcrypt($person['firstName']),
+
+            'locale' => 'nl',
+
+            'role' => UserRole::MEMBER,
+            'status' => UserStatus::ACTIVE,
+
+            'address' => faker()->address,
+            'postal' => faker()->postcode,
+            'city' => faker()->city,
+            'country' => faker()->country,
+            'telephone' => faker()->phoneNumber,
+        ]);
     }
 }
