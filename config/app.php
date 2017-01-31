09b3fa22f8501a392eed70d93f06e3dc5e9f824a
freek <freek@spatie.be>
2017-01-31 14:54:20 +0100

    commit

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         Spatie\TranslationLoader\TranslationServiceProvider::class,
         App\Services\Navigation\NavigationServiceProvider::class,
         App\Providers\ConfigServiceProvider::class,
-        //Themsaid\MailPreview\MailPreviewServiceProvider::class,
+        Themsaid\MailPreview\MailPreviewServiceProvider::class,
 
         App\Providers\AuthServiceProvider::class,
         App\Providers\CollectionServiceProvider::class,
