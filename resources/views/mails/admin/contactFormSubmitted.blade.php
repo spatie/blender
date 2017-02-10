0b0329565e97374a2b5ba281efd712cc3e519c35
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-02-10 11:03:25 +0100

    Add referer to form response mail

--- resources/views/mails/admin/contactFormSubmitted.blade.php
+++ resources/views/mails/admin/contactFormSubmitted.blade.php
@@ -13,6 +13,7 @@ Adres: {{ $formResponse->address }} <br>
 Postcode: {{ $formResponse->postal }} <br>
 Stad: {{ $formResponse->city }} <br>
 Opmerkingen: {{ $formResponse->remarks }} <br>
+Verwijzer: {{ $formResponse->referer ?? 'Onbekend' }} <br>
 @endcomponent
 
 @slot('subcopy')
