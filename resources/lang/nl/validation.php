a9cf01db65156d2a3a1fdb97f7ed09366ce59d11
freek <freek@spatie.be>
2017-02-21 10:24:03 +0100

    commit

--- resources/lang/nl/validation.php
+++ resources/lang/nl/validation.php
@@ -2,5 +2,6 @@
 
 return [
     'failedForm' => 'Corrigeer de fouten in het formulier',
-    'required' => 'Dit is een verplicht veld'
+    'required' => 'Dit is een verplicht veld',
+    'unique' => 'Er bestaat al een gebruiker met dit e-mailadres'
 ];
