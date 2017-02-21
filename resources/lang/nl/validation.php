e73c44e4b3cd6d92d12e445975fc9c07ea974ddc
freek <freek@spatie.be>
2017-02-21 10:56:08 +0100

    add missing translation

--- resources/lang/nl/validation.php
+++ resources/lang/nl/validation.php
@@ -3,5 +3,6 @@
 return [
     'failedForm' => 'Corrigeer de fouten in het formulier',
     'required' => 'Dit is een verplicht veld',
-    'unique' => 'Er bestaat al een gebruiker met dit e-mailadres'
+    'unique' => 'Er bestaat al een gebruiker met dit e-mailadres',
+    'confirmed' => 'U moet tweemaal hetzelfde wachtwoord ingeven'
 ];
