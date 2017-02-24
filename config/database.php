5a5fcc7ef134fefbf081d1420b4ab16ad7f23109
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-02-24 17:55:55 +0100

    wip

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_DEFAULT_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -48,7 +48,7 @@ return [
 
         'sqlite' => [
             'driver'   => 'sqlite',
-            'database' => storage_path().'/database.sqlite',
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix'   => '',
         ],
 
---
+++ tests/Features/Front/TestCase.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tests\Features\Front;
+
+use Tests\Features\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+}
