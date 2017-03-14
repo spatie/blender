cea5d73de309d0525c51beda043dd5297c6c463f
Freek Van der Herten <freek@spatie.be>
2017-03-14 23:32:40 +0100

    Apply fixes from StyleCI (#303)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,10 +4,10 @@ namespace Tests;
 
 use ArticleSeeder;
 use FragmentSeeder;
-use Illuminate\Contracts\Console\Kernel;
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -22,7 +22,7 @@ abstract class TestCase extends BaseTestCase
 
     public function createApplication()
     {
-        $app = require __DIR__ . '/../bootstrap/app.php';
+        $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
 
@@ -54,6 +54,7 @@ abstract class TestCase extends BaseTestCase
                 if (isset($pattern, $data) !== true) {
                     return null;
                 }
+
                 return preg_match(sprintf('%1$s%2$s%1$s%3$s', $delimiter, $pattern, $modifiers), $data) > 0;
             }
         );
