f506db7b104a5a8dbf00d54bcc0dbbfa8b355c69
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-03-15 11:35:23 +0100

    Dusk

--- tests/Browser/TestCase.php
+++ tests/Browser/TestCase.php
@@ -4,25 +4,25 @@ namespace Tests\Browser;
 
 use ArticleSeeder;
 use FragmentSeeder;
-use Tests\Concerns\UsesDatabase;
 use Tests\Concerns\CreatesApplication;
 use Laravel\Dusk\TestCase as BaseTestCase;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Tests\Concerns\UsesMySqlDatabase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    use UsesDatabase;
+    use UsesMySqlDatabase;
 
     public function setUp()
     {
         parent::setUp();
 
-        $this->setUpDatabase();
-
-        $this->artisan('db:seed', ['--class' => ArticleSeeder::class]);
-        $this->artisan('db:seed', ['--class' => FragmentSeeder::class]);
+        $this->setUpDatabase(function () {
+            $this->artisan('db:seed', ['--class' => ArticleSeeder::class]);
+            $this->artisan('db:seed', ['--class' => FragmentSeeder::class]);
+        });
     }
 
     /**
