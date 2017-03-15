a0db44841a04fc01dbe3823c5d6b68060e765e57
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-03-15 11:05:22 +0100

    wip

--- tests/Concerns/UsesDatabase.php
+++ tests/Concerns/UsesDatabase.php
@@ -4,14 +4,9 @@ namespace Tests\Concerns;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Contracts\Console\Kernel;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 trait UsesDatabase
 {
-    use DatabaseTransactions;
-
-    protected static $migrated = false;
-
     protected function setUpDatabase(callable $afterMigration = null)
     {
         $this->setUpSqlite();
@@ -23,8 +18,6 @@ trait UsesDatabase
         if ($afterMigration) {
             $afterMigration();
         }
-
-        static::$migrated = true;
     }
 
     protected function setUpSqlite()
@@ -38,20 +31,5 @@ trait UsesDatabase
                 return preg_match(sprintf('%1$s%2$s%1$s%3$s', $delimiter, $pattern, $modifiers), $data) > 0;
             }
         );
-    }
-
-    protected function beginDatabaseTransaction()
-    {
-        $database = $this->app->make('db');
-
-        foreach ($this->connectionsToTransact() as $name) {
-            $database->connection($name)->beginTransaction();
-        }
-
-        $this->beforeApplicationDestroyed(function () use ($database) {
-            foreach ($this->connectionsToTransact() as $name) {
-                $database->connection($name)->rollBack();
-            }
-        });
     }
 }
