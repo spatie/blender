e69e8acef72314cb5001004910b6b54f8c85644e
Sebastian De Deyne <sebastian@spatie.be>
2017-04-27 16:22:47 +0200

    CS

--- resources/views/front/_layouts/_partials/head/seo.blade.php
+++ resources/views/front/_layouts/_partials/head/seo.blade.php
@@ -5,13 +5,11 @@
 
 <title>{{ meta()->title() }}</title>
 
-{{ meta()
-    ->with([
-        'description' => __('site.description'),
-        'og:url' => request()->url(),
-        'og:title' => meta()->title(),
-        'og:description' => __('site.description'),
-        'og:type' => 'website',
-        'og:image' => url('/images/og-image.png'),
-    ])
-    ->with($meta ?? []) }}
+{{ meta()->with([
+    'description' => __('site.description'),
+    'og:url' => request()->url(),
+    'og:title' => meta()->title(),
+    'og:description' => __('site.description'),
+    'og:type' => 'website',
+    'og:image' => url('/images/og-image.png'),
+])->with($meta ?? []) }}
