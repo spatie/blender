793191b366a579bcca19315d6abceb29e2bcb895
Freek Van der Herten <freek@spatie.be>
2017-08-07 18:46:07 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Back/Api/ContentBlockController.php
+++ app/Http/Controllers/Back/Api/ContentBlockController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers\Back\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\ContentBlock;
-use App\Models\Transformers\ContentBlockTransformer;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Validation\ValidationException;
+use App\Models\Transformers\ContentBlockTransformer;
 
 class ContentBlockController extends Controller
 {
