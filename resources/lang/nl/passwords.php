36ea8308339f3d1a91e44e6faf12445686b9ef01
freek <freek@spatie.be>
2017-09-07 01:08:00 +0200

    fix message

--- resources/lang/nl/passwords.php
+++ resources/lang/nl/passwords.php
@@ -4,4 +4,5 @@ return [
     'invalidToken' => 'Deze link is ongeldig',
     'sent' => 'Een mail met een link waarmee u kan inloggen is verzonden naar uw emailadres.',
     'reset' => 'Uw wachtwoord werd opgeslagen',
+    'user' => 'Er is geen gebruiker met dit emailadres',
 ];
