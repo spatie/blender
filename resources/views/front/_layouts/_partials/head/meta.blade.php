5191aa61b22bea4fc421392c4101cd366a27ebc7
Willem Van Bockstal <willem@spatie.be>
2017-09-14 16:46:10 +0200

    Add viewport-fit for iPhone X

--- resources/views/front/_layouts/_partials/head/meta.blade.php
+++ resources/views/front/_layouts/_partials/head/meta.blade.php
@@ -1,3 +1,3 @@
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover">
 <link rel="canonical" href="@yield('canonical', request()->url())" />
