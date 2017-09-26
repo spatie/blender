552c2fddb5aa6277157e032eda8001bd2a6ca231
Alex Vanderbist <alex.vanderbist@gmail.com>
2017-09-26 16:54:45 +0200

    Use `slug` for SpecialArticle routes
    `url` property includes the locale and causes double locale segments in the route url

--- routes/front.php
+++ routes/front.php
@@ -4,8 +4,8 @@ use App\Models\Enums\SpecialArticle;
 
 Route::name('home')->get('/', 'HomeController@index');
 
-Route::name('contact')->get(article(SpecialArticle::CONTACT)->url, 'ContactController@index');
-Route::post(article(SpecialArticle::CONTACT)->url, 'ContactController@handleResponse');
+Route::name('contact')->get(article(SpecialArticle::CONTACT)->slug, 'ContactController@index');
+Route::post(article(SpecialArticle::CONTACT)->slug, 'ContactController@handleResponse');
 
 Route::get('{articleUrl}/{subArticleUrl}', 'ArticleController@index');
 Route::get('{articleUrl}', 'ArticleController@index');
