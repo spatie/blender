278cb2d0e7cd7c8e7cf2fd80f0fb12b71499b8b1
Harish Toshniwal <haristoshniwal@gmail.com>
2017-10-11 13:59:30 +0530

    Fix content blocks error on new record creation (#454)
    * fix
    
    * commit
    
    * Apply fixes from StyleCI
    
    * commit

--- app/Http/Controllers/Back/Api/ContentBlockController.php
+++ app/Http/Controllers/Back/Api/ContentBlockController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Back\Api;
 use App\Http\Controllers\Controller;
 use App\Models\ContentBlock;
 use App\Models\Transformers\ContentBlockTransformer;
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
@@ -26,7 +27,15 @@ class ContentBlockController extends Controller
 
     protected function getModelFromRequest($request): Model
     {
-        return call_user_func($request['model_name'].'::findOrFail', $request['model_id']);
+        if (! isset($request['model_name'])) {
+            throw new Exception('No model name provided');
+        }
+
+        if (! isset($request['model_id'])) {
+            throw new Exception('No model id provided');
+        }
+
+        return $request['model_name']::withoutGlobalScopes()->findOrFail($request['model_id']);
     }
 
     protected function validationRules(): array
