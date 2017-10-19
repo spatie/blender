4d75913ad3141bbd4089c65e027150f48b97db00
freek <freek@spatie.be>
2017-10-20 01:16:41 +0200

    install request logger

--- routes/front.php
+++ routes/front.php
@@ -5,7 +5,7 @@ use App\Models\Enums\SpecialArticle;
 Route::get('/', 'HomeController@index')->name('home');
 
 Route::get(article(SpecialArticle::CONTACT)->slug, 'ContactController@index')->name('contact');
-Route::post(article(SpecialArticle::CONTACT)->slug, 'ContactController@handleResponse');
+Route::post(article(SpecialArticle::CONTACT)->slug, 'ContactController@handleResponse')->middleware('logRequest');
 
 Route::get('{articleUrl}/{subArticleUrl}', 'ArticleController@index');
 Route::get('{articleUrl}', 'ArticleController@index');
