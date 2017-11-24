cc68f1ffb4135064039c3f079b0c17583976c0c9
Alex Vanderbist <alex.vanderbist@gmail.com>
2017-11-24 17:54:08 +0100

    Fix comment style

--- config/forms.php
+++ config/forms.php
@@ -1,18 +1,17 @@
 <?php
 
 return [
-
     /*
-    | This will be returned by the `recipients()` helper for all forms in
-    | non-production environments.
-    */
+     * This will be returned by the `recipients()` helper for all forms in
+     * non-production environments.
+     */
     'development_recipients' => [
         ['email' => 'technical@spatie.be', 'name' => 'SPATIE Technical'],
     ],
 
     /*
-    | Form types used by recipients.
-    */
+     * Form types used by recipients.
+     */
     'types' => [
         'contactForm' => 'Contact form',
     ],
