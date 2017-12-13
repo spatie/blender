a3608c7a0e4b3336ba52d8ad46a0d9161275c601
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-12-13 12:50:56 +0100

    Add Purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,7 @@
+const path = require('path');
+const glob = require('glob-all');
 const { mix } = require('laravel-mix');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
 
 mix.autoload({
     'jquery': ['$', 'window.jQuery', 'jQuery'],
@@ -40,4 +43,40 @@ mix
             // need to worry about ignoring them in our version control system.
             chunkFilename: 'js/[name].js',
         },
+
+        plugins: [
+            new PurgecssPlugin({
+                paths: glob.sync([
+                    path.join(__dirname, 'app/**/*.php'),
+                    path.join(__dirname, 'resources/views/**/*.blade.php'),
+                    path.join(__dirname, 'resources/assets/js/**/*.vue'),
+                    path.join(__dirname, 'resources/assets/js/**/*.js'),
+                    path.join(__dirname, 'vendor/spatie/menu/**/*.php'),
+
+                    // Blender css paths. In the future it would be preferable
+                    // to simply ignore all of blender-css by extracting all
+                    // selectors from the dist .css file provided by the
+                    // package, but it's currently not possible to add css
+                    // paths to Purgecss via the webpack plugin.
+                    path.join(__dirname, 'node_modules/@spatie/blender-css/**/*.scss'),
+                    path.join(__dirname, 'node_modules/datatables/**/*.js'),
+                    path.join(__dirname, 'node_modules/jquery-confirm/**/*.js'),
+                    path.join(__dirname, 'node_modules/select2/**/*.js'),
+                ]),
+                whitelistPatterns: [
+                    /fa-/, // FontAwesome icon font selectors
+                    /re-/, // Redactor icon font selectors
+                ],
+                extractors: [
+                    {
+                        extractor: class {
+                            static extract(content) {
+                                return content.match(/[A-z0-9-:\/]+/g) || [];
+                            }
+                        },
+                        extensions: ['html', 'js', 'php', 'scss', 'vue'],
+                    },
+                ],
+            }),
+        ],
     });
