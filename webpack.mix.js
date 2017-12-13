eab5b850b28aa3a91704516d1ad142002095e4c9
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-12-13 11:34:42 +0100

    Clean up webpack config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,10 @@
 const { mix } = require('laravel-mix');
 
-mix.config.postCss = require('./postcss.config').plugins;
-
 mix.autoload({
     'jquery': ['$', 'window.jQuery', 'jQuery'],
 });
 
 mix
-
     .js('resources/assets/js/front/head.js', 'public/js/front.head.js')
     .js('resources/assets/js/front/app.js', 'public/js/front.app.js')
     .postCss('resources/assets/css/front/front.css', 'public/css/front.css')
@@ -19,6 +16,14 @@ mix
     .version()
 
     .options({
+        // CSSNext already processes our css with Autoprefixer, so we don't
+        // need mix to do it twice.
+        autoprefixer: false,
+
+        // Our PostCSS plugins are defined in a standard `postcss.config.js`
+        // file, which we'll read for plugins.
+        postCss: require('./postcss.config').plugins,
+
         // Since we don't do any image preprocessing and write url's that are
         // relative to the site root, we don't want the css loader to try to
         // follow paths in `url()` functions.
@@ -30,6 +35,9 @@ mix
             // The public path needs to be set to the root of the site so
             // Webpack can locate chunks at runtime.
             publicPath: '/',
-            chunkFilename: 'js/[name]-[chunkhash].js',
+
+            // We'll place all chunks in the `js` folder by default so we don't
+            // need to worry about ignoring them in our version control system.
+            chunkFilename: 'js/[name].js',
         },
     });
